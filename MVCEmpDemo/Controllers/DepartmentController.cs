101ea9a9e868afe5f04b46a47ed9fae94605481b
mgoswamizc <mgoswami@zerochaos.com>
2015-10-08 05:08:29 -0400

    Add DepartmentController file

---
+++ MVCEmpDemo/Controllers/DepartmentController.cs
@@ -0,0 +1,53 @@
+﻿using MVCEmpDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Data.Entity;
+
+
+namespace MVCEmpDemo.Controllers
+{
+
+    public class DepartmentController : Controller
+    {
+        private MVCEntities dbCtx = new MVCEntities();
+
+        // GET: Department
+        public ActionResult Index()
+        {
+            return View(dbCtx.Departments.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Department dept)
+        {
+            if (!ModelState.IsValid) return View(dept);
+            var ExiastDept = dbCtx.Departments.Where(d => d.DeptName == dept.DeptName).Count();
+            if (ExiastDept == 0)
+            {
+                dbCtx.Departments.Add(dept);
+                dbCtx.SaveChanges();
+                return RedirectToAction("Index"); 
+            }
+            else
+            {
+                ViewBag.SomeError = "Department Is Already Added";
+                ViewBag.SucMsg = false;
+                return View();
+            }
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
